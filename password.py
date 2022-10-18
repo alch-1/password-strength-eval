0eadb61c68e1e6a4efcb841c5b1518d3d37a2d52
Al <bkup.12@protonmail.com>
2022-10-18 23:10:53 +0800

    v1.2

--- password.py
+++ password.py
@@ -86,16 +86,26 @@ def main():
       print(result)
       # r.span() => (7, 10)
       # r.group() => 'ttt'
-      # print(m.captures())
       return True
 
   ## date ##
-
+  def check_date(line):
+    datematch = regex.compile(r"^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]|(?:Jan|Mar|May|Jul|Aug|Oct|Dec)))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2]|(?:Jan|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec))\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)(?:0?2|(?:Feb))\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9]|(?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep))|(?:1[0-2]|(?:Oct|Nov|Dec)))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$")
+    result = datematch.search(line)
+    if result == None:
+      print("no match")
+      return False # no match
+    else:
+      print(result)
+      # r.span() => (7, 10)
+      # r.group() => 'ttt'
+      return True
   ## bruteforce ## 
+  
 
   # run strength checker
   with open("logs.txt", "a+") as f:
-    f.write("===" + TODAY + ", " + inp + "===")
+    f.write("===" + TODAY + ", " + inp + "===" + "\n")
     for line in lines:
       print(line)
       check_repeat(line)
